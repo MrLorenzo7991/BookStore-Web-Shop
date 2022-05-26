e5b3b6db9205e9340162b62e5380f1775e1b4f61
Alec <alemor98@hotmail.it>
2022-05-26 18:32:11 +0200

    Creazione modello per create e aggiunti altri medoti a controller book per sezione admin

--- BookStore-Web-Shop/Controllers/BookController.cs
+++ BookStore-Web-Shop/Controllers/BookController.cs
@@ -6,6 +6,7 @@ namespace BookStore_Web_Shop.Controllers
 {
     public class BookController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             using(BookStoreContext db = new BookStoreContext())
@@ -18,7 +19,8 @@ namespace BookStore_Web_Shop.Controllers
                 return View("ViewAdmin",booksCategories);
             }
         }
-
+        
+        [HttpGet]
         public IActionResult Details(int id)
         {
             using(BookStoreContext db = new BookStoreContext())
@@ -28,6 +30,69 @@ namespace BookStore_Web_Shop.Controllers
                 if(book != null)
                 {
                     return View(book);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            using (BookStoreContext db = new BookStoreContext())
+            {
+               
+                Book book = new();
+                List<Category> categories = db.Categories.ToList();
+
+                BookCategories model = new(book, categories);
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookCategories data)
+        {
+            if (!ModelState.IsValid)
+            {
+                using (BookStoreContext db = new BookStoreContext())
+                {
+                    data.Book = new Book();
+                    data.Categories = data.Categories.ToList();
+
+                    return View("Create",data);
+                }
+            }
+
+            using (BookStoreContext db = new BookStoreContext())
+            {
+                Book bookToCreate = new();
+                bookToCreate.Title = data.Book.Title;
+                bookToCreate.Price = data.Book.Price;
+                bookToCreate.Description = data.Book.Description;
+                bookToCreate.CategoryId = data.Book.CategoryId;
+                bookToCreate.UrlImage = data.Book.UrlImage;
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            using(BookStoreContext db = new BookStoreContext())
+            {
+                Book? bookToRemove = db.Books.Find(id);
+                if(bookToRemove != null)
+                {
+                    db.Books.Remove(bookToRemove);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index", "Book");
                 }
                 else
                 {
---
+++ BookStore-Web-Shop/Models/BookCategories.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStore_Web_Shop.Models
+{
+    public class BookCategories
+    {
+        public Book Book { get; set; }
+        public List<Category> Categories { get; set; }
+
+        public BookCategories(Book book, List<Category> categories)
+        {
+            Book = book;
+            Categories = categories;
+        }
+        public BookCategories() { }
+    }
+}
