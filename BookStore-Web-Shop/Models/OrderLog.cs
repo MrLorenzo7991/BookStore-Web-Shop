ed8e2cdc5eaa6b6be39b36fddff3d7df9e8e166d
Lorenzo <lorenzo.miccio97@gmail.com>
2022-05-26 14:45:24 +0200

    Creati modelli SellLog e OrderLog

---
+++ BookStore-Web-Shop/Models/OrderLog.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore_Web_Shop.Models
+{
+    public class OrderLog
+    {
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderLog()
+        {
+        }
+        public OrderLog(DateTime date, int quantity)
+        {
+            Date = date;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ BookStore-Web-Shop/Models/SellLog.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore_Web_Shop.Models
+{
+    public class SellLog
+    {
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }   
+        
+        public SellLog()
+        {
+        }
+        public SellLog(DateTime date, int quantity)
+        {
+            Date = date;
+            Quantity = quantity;
+        }
+    }
+}
