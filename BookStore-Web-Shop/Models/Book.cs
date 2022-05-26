110ab831261eb34f0aa992bb57c5b0920f3b076c
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-05-26 15:03:29 +0200

    creato i modelli category dei Book

---
+++ BookStore-Web-Shop/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace BookStore_Web_Shop.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string UrlImage { get; set; }
+        public string Description { get; set; } 
+        public double Price { get; set; }
+        public int Quantity { get; set; } = 0;
+        public int NumberOfLikes { get; set; } = 0;
+
+        public Book()
+        {
+        }
+
+        public Book(string title, string urlImage, string description, double price, int numberOfLikes)
+        {
+            Title = title;
+            UrlImage = urlImage;
+            Description = description;
+            Price = price;
+            NumberOfLikes = numberOfLikes;
+        }
+    
+    }
+
+}
---
+++ BookStore-Web-Shop/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore_Web_Shop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category()
+        {
+        }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
