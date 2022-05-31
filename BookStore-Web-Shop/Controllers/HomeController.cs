0e1c62ae319ed4c6ea50b9f8bc9adb326fa52d03
Lorenzo <lorenzo.miccio97@gmail.com>
2022-05-31 17:13:17 +0200

    Completato pagina di acquisto eseguito

--- BookStore-Web-Shop/Controllers/HomeController.cs
+++ BookStore-Web-Shop/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace BookStore_Web_Shop.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult SellSuccess()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
