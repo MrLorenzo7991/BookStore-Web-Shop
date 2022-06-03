6b5a4007441cc713e48e2d9d5e0ba59b53c35a7f
Lorenzo <lorenzo.miccio97@gmail.com>
2022-06-03 17:24:28 +0200

    bozza carosello best seller

--- BookStore-Web-Shop/Controllers/Api/BookApiController.cs
+++ BookStore-Web-Shop/Controllers/Api/BookApiController.cs
@@ -85,8 +85,10 @@ namespace BookStore_Web_Shop.Controllers.Api
             {
                 List<SellLog> sellLogs = db.SellLog.Include(selllog=>selllog.Book).Include(selllog=>selllog.Book.Category).ToList();
 
+
                 var obj = db.SellLog.Where(x => x.Date > DateTime.Now.AddDays(-30)).GroupBy(x => x.BookId)
                     .Select(x => new { Bookid = x.Key, Sum = x.Sum(item => item.Quantity) }).OrderByDescending(x => x.Sum).ToList();
+
 
                 //New crea un oggetto anonimo, non capisco come fa, ma ha 2 interi come attributi,
                 //forse creando un modello del genere si potrebbe usare, per JS funziona perchè quello non fa domando ma volevo fare il calcolo qui
