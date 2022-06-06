1e50ac89cffa1a4dd719f07085d6475b9598e683
MarinaAnto <antonellaarmas160@gmail.com>
2022-06-06 11:58:46 +0200

    .

---
+++ BookStore-Web-Shop/Validation/NotEmpty.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BookStore_Web_Shop.Validation
+{
+    public class NotEmpty: ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string element= (string)value;
+            if (element == "")
+            {
+                return new ValidationResult("Questo campo non può essere vuoto");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
+
+
+
